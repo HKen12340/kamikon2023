e601e1bc619cf2229aa3fd90376feb47f4d7e163
unknown <n1210172@jn2.iwasaki.ac.jp>
2023-06-15 19:35:11 +0900

    ヘッダー作成

---
+++ components/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="../style.css" type="text/css">
+    <title>Cooking Cross</title>
+    <style>
+      @charset "UTF-8";
+        * {	box-sizing:border-box;}
+
+        body {
+        margin:0;
+        }
+
+        .header{
+          background:#4169e1;
+          padding-left:20px;
+        }
+
+        .header > .container {
+          height:90px;
+          display:flex;
+          color:#fff;
+          align-items:center;
+        }
+
+        .title {
+          font-size:30px;
+          margin-right:20px;
+          margin-left:20px;
+        }
+        .logo{
+          width:100px;
+          height:80px;
+          margin-right:auto;
+        }
+    </style>
+  </head>
+  <body>
+    <header class="header">
+      <div class="container">
+        <div class="title">Cooking Cross</div>
+        <img class="logo" src="http://localhost/kamikon2023/components/image/sample_logo.png" alt="ロゴ">
+      </div>
+    </header>
+  </body>
+</html>
