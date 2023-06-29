70a6af8e74e73d685ccc1db8bbcae056415a3fb4
yaka2021 <cbky1211051@gn.iwasaki.ac.jp>
2023-06-29 21:56:51 +0900

    new

--- login.view.php
+++ login.view.php
@@ -5,18 +5,24 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 <title>Cooking-Cross</title>
 </head>
+
 <body>
 
-<a href="" class="new">アカウントをお持ちでない方はこちら</a>
+<?php
+require './components/header.php';
+			?>
+
+
+<a href="" class="loginview_new">アカウントをお持ちでない方はこちら</a>
 
 <form name="login_form">
-  <div class="login_form_btm">
-　　<p class = "mlad">メールアドレス</p>
-  <input type="id" name="user_id" class="ip1"><br>
-    <p class = "pswd">パスワード</p>
-    <input type="password" name="password" class="ip2">
+  <div>
+　　<p class = "loginview_mlad">メールアドレス</p>
+  <input type="id" name="user_id" class="loginview_ip1"><br>
+    <p class = "loginview_pswd">パスワード</p>
+    <input type="password" name="password" class="loginview_ip2">
   </div>
-  <button type="submit" class="rgin">ログイン</button>
+  <button type="submit" class="loginview_rgin">ログイン</button>
 </form>
 
 
@@ -24,6 +30,4 @@
 
 </html>
 
-<?php
-require './components/header.php';
-			>
+
