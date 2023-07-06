0d68329e63e8925ada47511b418a58039228636f
yaka2021 <cbky1211051@gn.iwasaki.ac.jp>
2023-07-06 21:30:45 +0900

    処理を追加。

--- login.view.php
+++ login.view.php
@@ -6,19 +6,28 @@
   <title>Cooking-Cross</title>
 </head>
 <body>
-<?php require './components/header.php';	?>
+<?php require './components/header.php';	
+      require './config/user_model.php';
 
-<form name="login_form">
+      if(!empty($_POST)){
+      $user = new User();
+      if($user->user_login($_POST['email'],$_POST['password'])){
+        header('Location: login_db.php');
+      }
+    }
+?>
+
+<form name="login_form" method="post">
   <div class="loginview_form">
     <div>
       <p class = "loginview_mlad">メールアドレス</p>
-      <input type="id" name="user_id" class="loginview_ip1"><br>
+      <input type="email" name="email" class="loginview_ip1"><br>
       <p class = "loginview_pswd">パスワード</p>
       <input type="password" name="password" class="loginview_ip2">
     </div>
     <button type="submit" class="loginview_rgin">ログイン</button>
   </div>
 </form>
-<a href="" class="loginview_new">アカウントをお持ちでない方はこちら</a>
+<a href="./account/regist_account.php" class="loginview_new">アカウントをお持ちでない方はこちら</a>
 </body>
 </html>
