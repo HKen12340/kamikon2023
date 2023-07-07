604d72a46adc1d2f569f8b0a54d4926d9e10a9e8
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2023-07-07 12:16:13 +0900

    修正2

--- config/recipe_model.php
+++ config/recipe_model.php
@@ -7,4 +7,27 @@ class Recipe_model extends DB_connect{
   {
     parent::__construct();
   }
+
+  //ここから
+  public function view_recipe(){
+    // try{
+    //   $sql = 'select * from user where id = :id';
+    //   $stmt = $this->pdo->prepare($sql);
+    //   $stmt->bindValue(':id',$id);
+    //   $stmt->execute();
+    //   $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    //   return $result;
+    // }catch(PDOException $e){
+    //   echo "エラーが発生しました".$e->getMessage();
+    // }
+  }
+
+  public function create_recipe(){
+
+  }
+
+  public function delete_recipe(){
+
+  }
+
 }
