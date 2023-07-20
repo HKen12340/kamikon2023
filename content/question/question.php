97502f3272e777b88a84883cc89bf4a6f07092a3
yaka2021 <cbky1211051@gn.iwasaki.ac.jp>
2023-07-20 19:39:28 +0900

    質問画面を作成

---
+++ content/question/question.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="../../style.css">
+  <title>Cooking-Cross</title>
+</head>
+<body>
+    
+<?php require '../../components/header.php';	
+?>
+
+<div>
+  <p class="question_q1">今日は疲れている</p>
+  <button class="question_yes" type="submit">Yes</button>
+  <button class="question_no" type="submit">No</button>
+</div>
+</body>
+</html>
