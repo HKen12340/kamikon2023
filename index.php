4358e8a2c2c1317461a45e45e503ac68089d0839
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2023-08-04 20:28:03 +0900

    画面一覧 Cardデザイン作成

--- index.php
+++ index.php
@@ -15,9 +15,17 @@
   $recipe = new Recipe_model;
   $result = $recipe->get_recipeList(1);
   foreach($result as $res){
-    print '<a href = "./content/release_recipe/release_view/release_view.php?id='.$res["id"].'" class="index_f-item">'.'<img src='.$res["icon"].'>'.'</img>'.$res["recipe_name"].'</a>';
+    print '
+    <div class="index_f-item">
+      <a href = "./content/release_recipe/release_view/release_view.php?id='.$res["id"].'">'.
+        '<img src='.$res["icon"].'>'.'</img>'.$res["recipe_name"].'
+      </a>
+      <p>by '.$res['user_name'].'</p>
+    </div>';
  }
   ?>
 </div>
+<footer>
+</footer>
 </body>
 </html>
