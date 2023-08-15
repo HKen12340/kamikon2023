6dcfc08a2ac82c22788aba5ddc6f28ad566dfa1b
yaka2021 <cbky1211051@gn.iwasaki.ac.jp>
2023-08-15 19:16:15 +0900

    ページネーション関連の処理を追加。

--- index.php
+++ index.php
@@ -13,19 +13,61 @@
   <?php
   require("database/recipe_model.php");
   $recipe = new Recipe_model;
-  $result = $recipe->get_recipeList(1);
-  foreach($result as $res){
-    print '
-    <div class="index_f-item">
-      <a href = "./content/release_recipe/release_view/release_view.php?id='.$res["id"].'">'.
-        '<img src='.$res["icon"].'>'.'</img>'.$res["recipe_name"].'
-      </a>
-      <p>by '.$res['user_name'].'</p>
-    </div>';
- }
-  ?>
+ 
+ if(!isset($_GET['page_id'])){
+  $now = 1;
+}else{
+  $now = $_GET['page_id'];
+}
+$result = $recipe->get_recipeList($now);
+foreach($result as $res){
+  print '
+  <div class="index_f-item">
+    <a href = "./content/release_recipe/release_view/release_view.php?id='.$res["id"].'">'.
+      '<img src='.$res["icon"].'>'.'</img>'.$res["recipe_name"].'
+    </a>
+    <p>by '.$res['user_name'].'</p>
+  </div>';
+}
+
+ $max_page = $recipe->maxpage();
+
+if($now > 1){ 
+    print '<ul class="index_Pagination"><li class="index_Pagination-Item"><a class="index_Pagination-Item-Link" href="./index.php?page_id='.''.($now - 1).''.'"><svg xmlns="http://www.w3.org/2000/svg" class="index_Pagination-Item-Link-Icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+</svg></a></li>';
+} else {
+    print '<ul class="index_Pagination"><li class="index_Pagination-Item"><div class="index_Pagination-Item-Link isActive"><svg xmlns="http://www.w3.org/2000/svg" class="index_Pagination-Item-Link-Icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+</svg><div></li>';
+}
+  
+for($i = 1; $i <= $max_page; $i++){
+    if ($i == $now) {
+        print ' <li class="index_Pagination-Item"> <div class="index_Pagination-Item-Link isActive">'.'<span>'. $i.'</span>'; 
+    } else {
+        print ' <li class="index_Pagination-Item"> <a class="index_Pagination-Item-Link" href="index.php?page_id='.''.$i.''.' ">'.'<span>'. $i.'</span>'. '</a>';
+    }
+}
+
+if($now < $max_page){
+  print ' <li class="index_Pagination-Item">
+  <a class="index_Pagination-Item-Link" href="./index.php?page_id='.''.$max_page.''.'">
+      <svg xmlns="http://www.w3.org/2000/svg" class="index_Pagination-Item-Link-Icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+      </svg>
+  </a>
+</li>
+</ul>';} else{print '<li class="index_Pagination-Item"><div class="index_Pagination-Item-Link isActive"><svg xmlns="http://www.w3.org/2000/svg" class="index_Pagination-Item-Link-Icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+</svg>
 </div>
-<footer>
-</footer>
+</li>
+</ul>';}
+
+
+  ?>
+
+</div>
 </body>
 </html>
