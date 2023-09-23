9048a99684f3b056f7b90d1b5b9da1cd921202b7
Kenta12340 <86399751+Kenta12340@users.noreply.github.com>
2023-09-23 23:23:20 +0900

    質問機能 スタート画面 作成

---
+++ content/question/js/question.js
@@ -0,0 +1,121 @@
+//質問の処理ここから
+const APIURL = "http://localhost/kamikon2023/content/question/api.php";
+const POSTAPIURL = "http://localhost/kamikon2023/content/question/PostApi.php";
+const RESULT_URL = "http://localhost/kamikon2023/content/question/result.php";
+
+let data = {
+  time: 0,
+  money: 0,
+  volume:0,
+  meat:0,
+  fish:0,
+  vegetable:0
+};
+
+let question_Ele = document.getElementById("question_q1");
+
+let question_yes = document.getElementById("question_yes");
+let question_no = document.getElementById("question_no");
+let pointsObj = data;
+
+let counter = 1;
+window.onload = function() {
+  fetch(APIURL + '?ques=' + counter)
+  .then(
+    (response) => response.json()
+   )
+  .then((json) => {
+    console.log(json)
+    question_Ele.innerHTML = json.question;
+    question_yes.innerHTML = json.YesButtonText;
+    question_no.innerHTML = json.NoButtonText;
+    counter++;
+  }
+  );  
+}
+
+question_yes.addEventListener('click',() => {
+  QuestionCounter();
+  fetch(APIURL + '?ques=' + counter)
+  .then(
+    (response) => response.json()
+   )
+  .then((json) => {
+    question_Ele.innerHTML = json.question;
+    question_yes.innerHTML = json.YesButtonText;
+    question_no.innerHTML = json.NoButtonText;
+    pointSave(json.YesPointType)
+  }
+  );
+},false)
+
+question_no.addEventListener('click',() => {
+  QuestionCounter()
+  fetch(APIURL + '?ques=' + counter)
+    .then(
+      (response) => response.json()
+    )
+    .then((json) => {
+      question_Ele.innerHTML = json.question;
+      question_yes.innerHTML = json.YesButtonText;
+      question_no.innerHTML = json.NoButtonText;
+      pointSave(json.NoPointType)
+    }
+  );
+},false)
+
+
+pointSave = (type) => {
+  //文字列をオブジェクトに変換
+  TypeArray = pointsObj.PointType;
+
+  //ポイント加算
+  type.map(function(value){
+    switch(value){
+      case "time":
+        pointsObj.time += 1
+        break
+      case "money":
+        pointsObj.money += 1
+        break
+      case "volume":
+        pointsObj.volume += 1
+        break
+      case "meat":
+        pointsObj.meat += 1
+        break
+      case "fish":
+        pointsObj.fish += 1
+        break
+      case "vegetable":
+        pointsObj.vegetable += 1
+        break
+    }
+})
+  counter++;
+}
+
+QuestionCounter = () =>{
+  if(counter > 5){
+    let postData = {
+      Point:pointsObj,
+      Type:DropTextArray
+    } 
+    //溜まったポイントを参考にレシピを提案
+    fetch(POSTAPIURL,{
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(postData)
+    }) .then(response => response.json()) // 返ってきたレスポンスをjsonで受け取って次のthenへ渡す
+    .then(json => {
+      //結果をCookieに保存
+        document.cookie = "Ques_ansawer = " + json;
+        location.href = RESULT_URL;
+    })
+    .catch(error => {
+        console.log(error); // エラー表示
+    });
+  }
+}
--- content/question/question.php
+++ content/question/question.php
@@ -7,51 +7,83 @@
   <title>Cooking-Cross</title>
 </head>
 <style>
+  
 	#draggable1,#draggable2,#draggable3,#draggable4,#draggable5,#draggable6 {
 		width: 100px; height: 100px; padding: 0.5em;
     user-select: none;
 		margin: 10px 10px 10px 0;
+    text-align: center;
 	}
 	#droppable1,#droppable2,#droppable3,#droppable4,#droppable5,#droppable6 {
-		width: 150px; height: 150px; padding: 0.5em;
+		width: 120px; height: 120px; padding: 0.5em;
     user-select: none;
 		margin: 10px;
+    text-align: center;
 	}
+
 	.droparea{
-		margin: 5px;
-		display: flex;
+		margin: 0 auto;
+    width: 50%;
+    display: flex;
+    justify-content: space-around;
 	}
 	#dropContent{
-		margin: 5px;
+    margin: 0 auto;
+    width: 80%;
 		display: flex;
+    justify-content: space-around;
+    align-items: center;
 	}
 
   #ques_scene{
     display:none;
+    width: 70%;
+    margin: 0 auto;
+
+  }
+  #submit_Btn{
+    padding: 10px 60px;
+    /* background-color: blue; */
+    font-size: 20px;
+    border: none;
+    color: white;
+    border-radius: 5px;
+
+  }
+  #submit_Btn:hover{
+    /* background-color: aqua; */
+  }
+  .SubmitBtn_Area{
+    margin-top: 50px;
+    text-align: center;
   }
 </style>
 <body>
     
 <?php require '../../components/header.php';?>
+<div id="start_scene">
+  <h2>質問の解答に合わせて、最適なレシピを提案します</h2>
+  <button id = "QuesStartBtn">スタート</button>
+</div>
 <div id="draggable_scene">
+  <h3 class="Question_DragglbleH3">6つの要素を重要度の高い順にドラックアンドドロップで配置してください</h3>
   <section class="droparea">
-    
-      <div id="draggable1" class="ui-widget-content">
-        <p value="time_point">時間</p>
-        <input type="hidden" value="time_point">
-      </div>
-      <div id="draggable2" class="ui-widget-content">
-        <p>予算</p>
-        <input type="hidden" value="money_point">
-      </div>
-      <div id="draggable3" class="ui-widget-content">
-        <p>量</p>
-        <input type="hidden" value="volume_point">
-      </div>
-      <div id="draggable4" class="ui-widget-content">
-        <p>野菜</p>
-        <input type="hidden" value="vegetable_point">
-      </div>
+    <div id="draggable1" class="ui-widget-content">
+      <p value="time_point">時間</p>
+      <input type="hidden" value="time_point">
+    </div>
+    <div id="draggable2" class="ui-widget-content">
+      <p>予算</p>
+      <input type="hidden" value="money_point">
+    </div>
+    <div id="draggable3" class="ui-widget-content">
+      <p>量</p>
+      <input type="hidden" value="volume_point">
+    </div>
+    <div id="draggable4" class="ui-widget-content">
+      <p>野菜</p>
+      <input type="hidden" value="vegetable_point">
+    </div>
       <div id="draggable5" class="ui-widget-content">
         <p>肉</p>
         <input type="hidden" value="meat_point">
@@ -75,7 +107,6 @@
     <div id="droppable4" class="ui-widget-header">
       <p>4</p>
     </div>
-
     <div id="droppable5" class="ui-widget-header">
       <p>5</p>
     </div>
@@ -83,289 +114,32 @@
       <p>6</p>
     </div>
   </section>
-  <button id="submit_Btn" disabled>送信</button>
+  <section class="SubmitBtn_Area">
+    <button id="submit_Btn" disabled>送信</button>
+  </section>
 </div>
 
 <div id="ques_scene">
   <p id="question_q1"></p>
-  <button id="question_yes" type="submit">Yes</button>
-  <button id="question_no" type="submit">No</button>
+  <div class="question_YesNoButtonArea">
+    <button id="question_yes" type="submit">Yes</button>
+    <button id="question_no" type="submit">No</button>
+  </div>
 </div>
+<script>
+  let start_scene = document.getElementById("start_scene");
+  let draggable_scene = document.getElementById("draggable_scene");
+  let QuesStartBtn = document.getElementById("QuesStartBtn");
+  draggable_scene.style.display= "none";
 
+  QuesStartBtn.addEventListener("click",function(){
+    start_scene.style.display= "none";
+    draggable_scene.style.display= "block";
+  },false)
+</script>
 <script src="../../asset/jquery-3.7.0.min.js"></script>
 <script src="../../asset/jquery-ui.min.js"></script>
-<script>
-
-//ドラック&ドロップ
-let DropSubmitBtn =document.getElementById("submit_Btn");
-let DropTextArray = [];
-$( function() {
-		$( "#draggable1" ).draggable();
-		$( "#draggable2" ).draggable();
-		$( "#draggable3" ).draggable();
-		$( "#draggable4" ).draggable();
-		$( "#draggable5" ).draggable();
-		$( "#draggable6" ).draggable();
-
-		$( "#droppable1" ).droppable({
-			drop: function( event, ui ) {
-        DropTextArray[0] = ui.draggable.find('input').val()
-        dropCount()
-				$( this )
-				.addClass( "ui-state-highlight" )
-				.find( "p" )
-				.html( "ドロップされた！" );
-        
-			},
-			out: function(event,ui){
-        DropTextArray[0] = ''
-        dropCount()
-				$( this )
-				.removeClass("ui-state-highlight")
-				.find( "p" )
-				.html( "ここにドロップして下さい" )
-			}
-		});
-
-		$( "#droppable2" ).droppable({
-			drop: function( event, ui ) {
-        DropTextArray[1] = ui.draggable.find('input').val()
-        dropCount()
-				$( this )
-				.addClass( "ui-state-highlight" )
-				.find( "p" )
-				.html( "ドロップされた！" );
-			},
-			out: function(event,ui){
-        DropTextArray[1] = ''
-        dropCount()
-				$( this )
-				.removeClass("ui-state-highlight")
-				.find( "p" )
-				.html( "ここにドロップして下さい" )
-			}		
-		});
-
-		$( "#droppable3" ).droppable({
-			drop: function( event, ui ) {
-				DropTextArray[2] = ui.draggable.find('input').val()
-        dropCount()
-				$( this )
-				.addClass( "ui-state-highlight" )
-				.find( "p" )
-				.html( "ドロップされた！" );
-			},
-			out: function(event,ui){
-        DropTextArray[2] = ''
-        dropCount()
-				$( this )
-				.removeClass("ui-state-highlight")
-				.find( "p" )
-				.html( "ここにドロップして下さい" )
-			}
-		});
-
-		$( "#droppable4" ).droppable({
-			drop: function( event, ui ) {
-          DropTextArray[3] = ui.draggable.find('input').val()
-					dropCount()
-				$( this )
-				.addClass( "ui-state-highlight" )
-				.find( "p" )
-				.html( "ドロップされた！" );
-			},
-			out: function(event,ui){
-        DropTextArray[3] = ''
-        dropCount()
-				$( this )
-				.removeClass("ui-state-highlight")
-				.find( "p" )
-				.html( "ここにドロップして下さい" )
-			}
-		});
-
-		$( "#droppable5" ).droppable({
-			drop: function( event, ui ) {
-        DropTextArray[4] = ui.draggable.find('input').val()
-				dropCount()
-				$( this )
-				.addClass( "ui-state-highlight" )
-				.find( "p" )
-				.html( "ドロップされた！" );
-			},
-			out: function(event,ui){
-        DropTextArray[4] = ''
-        dropCount()
-				$( this )
-				.removeClass("ui-state-highlight")
-				.find( "p" )
-				.html( "ここにドロップして下さい" )
-			}
-		});
-
-		$( "#droppable6" ).droppable({
-			drop: function( event, ui ) {
-				DropTextArray[5] = ui.draggable.find('input').val()
-        dropCount()
-				$( this )
-				.addClass( "ui-state-highlight" )
-				.find( "p" )
-				.html( "ドロップされた！" );
-			},
-			out: function(event,ui){
-        DropTextArray[5] = ''
-        dropCount()
-				$( this )
-				.removeClass("ui-state-highlight")
-				.find( "p" )
-				.html( "ここにドロップして下さい" )
-			}
-		});
-	});
-
- //全て配置したか確認
-  dropCount = () => {
-        FlagArray = DropTextArray.filter(function(value){
-            return value != '';
-        })
-
-        if(FlagArray.length == 6){
-          DropSubmitBtn.disabled = false;
-        }else{
-          DropSubmitBtn.disabled = true;
-        }
-  }
-
-  //送信ボタン
-  DropSubmitBtn.addEventListener('click',() => {
-    document.getElementById('draggable_scene').style.display = "none";
-    document.getElementById('ques_scene').style.display = "block";
-  },false)
-
-//質問の処理ここから
-let data = {
-  time: 1,
-  money: 1,
-  volume:1,
-  meat:1,
-  fish:1,
-  vegetable:1
-};
-
-let question_Ele = document.getElementById("question_q1");
-
-let question_yes = document.getElementById("question_yes");
-let question_no = document.getElementById("question_no");
-let pointsObj = data;
-
-let counter = 1;
-window.onload = function() {
-  fetch('http://localhost/kamikon2023/content/question/api.php?ques=' + counter)
-  .then(
-    (response) => response.json()
-   )
-  .then((json) => {
-    console.log(json)
-    question_Ele.innerHTML = json.question;
-    question_yes.innerHTML = json.YesButtonText;
-    question_no.innerHTML = json.NoButtonText;
-    counter++;
-  }
-  );  
-}
-
-question_yes.addEventListener('click',() => {
-  QuestionCounter();
-  fetch('http://localhost/kamikon2023/content/question/api.php?ques=' + counter)
-  .then(
-    (response) => response.json()
-   )
-  .then((json) => {
-    question_Ele.innerHTML = json.question;
-    question_yes.innerHTML = json.YesButtonText;
-    question_no.innerHTML = json.NoButtonText;
-    pointSave(json.YesPointType)
-  }
-  );
-},false)
-
-question_no.addEventListener('click',() => {
-  QuestionCounter()
-  fetch('http://localhost/kamikon2023/content/question/api.php?ques=' + counter)
-    .then(
-      (response) => response.json()
-    )
-    .then((json) => {
-      question_Ele.innerHTML = json.question;
-      question_yes.innerHTML = json.YesButtonText;
-      question_no.innerHTML = json.NoButtonText;
-      pointSave(json.NoPointType)
-    }
-  );
-},false)
-
-
-pointSave = (type) => {
-  //文字列をオブジェクトに変換
-  TypeArray = pointsObj.PointType;
-
-
-  type.map(function(value){
-    switch(value){
-      case "time":
-        pointsObj.time += 1
-        console.log('time')
-        break
-      case "money":
-        pointsObj.money += 1
-        console.log('money')
-        break
-      case "volume":
-        pointsObj.volume += 1
-        console.log('volume')
-        break
-      case "meat":
-        pointsObj.meat += 1
-        console.log('meat')
-        break
-      case "fish":
-        pointsObj.fish += 1
-        console.log('fish')
-        break
-      case "vegetable":
-        pointsObj.vegetable += 1
-        console.log('vegetable')
-        break
-    }
-})
-  console.log(pointsObj)
-  counter++;
-}
-
-QuestionCounter = () =>{
-  if(counter > 5){
-    let postData = {
-      Point:pointsObj,
-      Type:DropTextArray
-    } 
-    
-    fetch('http://localhost/kamikon2023/content/question/PostApi.php',{
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(postData)
-    }) .then(response => response.json()) // 返ってきたレスポンスをjsonで受け取って次のthenへ渡す
-    .then(json => {
-        console.log(json); // やりたい処理
-        document.cookie = "Ques_ansawer = " + json;
-        location.href = "http://localhost/kamikon2023/content/question/result.php";
-    })
-    .catch(error => {
-        console.log(error); // エラー表示
-    });
-  }
-}
-</script>
+<script src="js/draggable.js"></script>
+<script src="js/question.js"></script>
 </body>
 </html>
